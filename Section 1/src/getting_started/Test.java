f0e6a4f9339c51e2618dcade805d1851d995944f
AtassiCodes <atassisha@gmail.com>
2022-04-16 06:51:03 -0400

    test

---
+++ Section 1/src/getting_started/Test.java
@@ -0,0 +1,10 @@
+package getting_started;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// Printing something else
+		System.out.println("Hello, the weather is great!");
+	}
+
+}
